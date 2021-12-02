f9d8d08be34a893e428d472abf32d9657e4e423d
Tom Stroobants <tom.stroobants@cloudar.be>
2021-12-02 13:56:59 +0100

    Removed cdk.out and updated gitignore/npmignore

--- .projenrc.js
+++ .projenrc.js
@@ -5,6 +5,7 @@ const project = new AwsCdkConstructLibrary({
   cdkVersion: '1.129.0',
   defaultReleaseBranch: 'main',
   name: 'cloudwatch-auto-retention',
+  keywords: ['aws', 'cdk', 'cloudwatch', 'retention', 'loggroups'],
   repository: 'https://github.com/stroobants-dev/cloudwatch-auto-retention',
   repositoryUrl: 'https://github.com/stroobants-dev/cloudwatch-auto-retention.git',
   python: {
@@ -18,17 +19,20 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/aws-events-targets',
     '@aws-cdk/aws-iam',
   ], /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
-  cdkTestDependencies: [
-    '@aws-cdk/core',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-events',
-    '@aws-cdk/aws-events-targets',
-    '@aws-cdk/aws-iam',
-  ], /* AWS CDK modules required for testing. */
+  // cdkTestDependencies: [
+  //   '@aws-cdk/core',
+  //   '@aws-cdk/aws-lambda',
+  //   '@aws-cdk/aws-events',
+  //   '@aws-cdk/aws-events-targets',
+  //   '@aws-cdk/aws-iam',
+  // ], /* AWS CDK modules required for testing. */
   // deps: [],                        /* Runtime dependencies of this module. */
   // description: undefined,          /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                     /* Build dependencies for this module. */
   // packageName: undefined,          /* The "name" in package.json. */
   // release: undefined,              /* Add release management to this project. */
 });
+const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage'];
+project.gitignore.exclude(...common_exclude);
+project.npmignore.exclude(...common_exclude, 'images');
 project.synth();
